da48cc0b3ee41f09fc6cf77f71f22b854ed55ebd
Sidhdongre358 <sidhdongre358@gmail.com>
2024-12-26 18:27:40 +0530

    CORS configurations added for each service as globally and tested it end to end.

--- shipping-service/src/main/java/com/sdongre/shipping_service/ShippingServiceApplication.java
+++ shipping-service/src/main/java/com/sdongre/shipping_service/ShippingServiceApplication.java
@@ -8,6 +8,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 public class ShippingServiceApplication {
 
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(ShippingServiceApplication.class, args);
 	}
